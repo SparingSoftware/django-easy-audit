fe52966663e4d29253eec0ad83c90daaab91fea3
Piotr Nowakowski <p.nowakowski@p-programisci.pl>
2019-10-22 15:43:54 +0200

    Add ModelAdminWithHistory

--- easyaudit/tests/test_app/admin.py
+++ easyaudit/tests/test_app/admin.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from easyaudit.admin import ModelAdminWithHistory
+from test_app.models import TestModel
+
+
+class TestModelAdmin(ModelAdminWithHistory):
+    pass
+
+
+admin.site.register(TestModel, TestModelAdmin)
